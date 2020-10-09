cd9049a59c933b49143c3e32b286e4b59e4e895b
Druxys <drudrux@gmail.com>
2020-10-09 15:59:01 +0200

    adding jwt security

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -5,24 +5,30 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Exception\AlreadySubmittedException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class RegisterController extends AbstractController
 {
+
     /**
      * @Route("/register", name="register", methods={"POST"})
      * @param Request $request
      * @param ValidatorInterface $validator
+     * @param UserPasswordEncoderInterface $passwordEncoder
      * @return JsonResponse
+     * @throws AlreadySubmittedException
      */
-    public function registerUser(Request $request, ValidatorInterface $validator)
+    public function registerUser(Request $request, ValidatorInterface $validator, UserPasswordEncoderInterface $passwordEncoder)
     {
         $user = new User();
         $datas = json_decode($request->getContent(), true);
+        $datas['password'] = $passwordEncoder->encodePassword($user, $datas['password']);
         $formRegister = $this->createForm(RegisterFormType::class, $user);
 
         $formRegister->submit($datas);
